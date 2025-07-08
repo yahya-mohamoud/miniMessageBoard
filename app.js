0e7c693463ce7b1a02cf4af05107a2dd64aef116
yahya-mohamoud <yahyamohamoud625@gmail.com>
2025-07-08 10:45:40 +0300

    Finished the whole project

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import messageRouter from './routes/message.js';
 
 const app = express();
 
 dotenv.config()
 app.set("views",  "views")
-app.set("view engine", "views")
-
-app.get('/', (req, res) => {
-    res.send('Hello world')
-})
+app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 
-const PORT = process.env.PORT || 3000;
+app.use('/', messageRouter)
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log(`Server Started On PORT: ${PORT}`);
---
+++ routes/message.js
@@ -0,0 +1,36 @@
+import { Router } from "express";
+
+const messageRouter = Router()
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+messageRouter.get('/', (req, res) => {
+    // console.log('hi iam workind')
+    res.render('index', { title: "Mini Message Board", messages: messages})
+})
+
+messageRouter.get('/new', (req, res) => {
+    res.render('form', {title: "Mini Message Board"})
+})
+
+messageRouter.post('/new', (req, res) => {
+    const data = req.body;
+    const text = data.text;
+    const user = data.user;
+
+    messages.push({ text: text, user: user, added: new Date() })
+    res.redirect('/')
+})
+
+export default messageRouter;
